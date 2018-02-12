35d2dbb42267e2ca8fb2d8d8896e6e5a3776aa53
doubleaykay <the.a.cars@gmail.com>
2018-02-12 09:27:14 -0500

    initisl frontend
    functional implementation of cytoscape.js

---
+++ frontend/data/test.js
@@ -0,0 +1,13 @@
+var cy = cytoscape({
+  container: document.getElementById('cy'),
+  elements: [
+    { data: { id: 'a' } },
+    { data: { id: 'b' } },
+    {
+      data: {
+        id: 'ab',
+        source: 'a',
+        target: 'b'
+      }
+    }]
+});
