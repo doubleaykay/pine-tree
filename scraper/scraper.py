5451f6fd32e02d1771cf19d86f87f70e446c2fc2
LevineMa <Matthewlevine16@gmail.com>
2018-02-15 16:40:27 -0500

    Python Part
    This loops through all the class numbers and prints out the information currently for Chemistry but will be used for everything

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -1,9 +1,18 @@
 from lxml import html
 import requests
-# See if adding the string at the end by using + '75' works
-page = requests.get('http://dartmouth.smartcatalogiq.com/en/current/orc/Departments-Programs-Undergraduate/Chemistry/CHEM-Chemistry-Undergraduate/CHEM-' + '75')
-tree = html.fromstring(page.content)
-prereq = tree.xpath('//a[@class="sc-courselink"]/text()')
-title = tree.xpath('//span/text()')
-print(prereq)
-print(title)
+
+def print_class():
+  # Initializes class number at 1
+   class_number = 1
+   while class_number <= 100:
+    # Grabs the info from the page
+       page = requests.get('http://dartmouth.smartcatalogiq.com/en/current/orc/Departments-Programs-Undergraduate/Chemistry/CHEM-Chemistry-Undergraduate/CHEM-' + str(class_number))
+       class_number += 1
+   #page = requests.get('http://dartmouth.smartcatalogiq.com/en/current/orc/Departments-Programs-Undergraduate/Chemistry/CHEM-Chemistry-Undergraduate/CHEM-' + str(x))
+   tree = html.fromstring(page.content)
+   prereq = tree.xpath('//a[@class="sc-courselink"]/text()')
+   title = tree.xpath('//span/text()')
+   print(prereq)
+   #print(title)
+
+print_class()
