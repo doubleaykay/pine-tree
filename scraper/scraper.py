f02c7fd8e0a767145299312d92ab63f8cf5ff512
LevineMa <Matthewlevine16@gmail.com>
2018-02-15 16:28:12 -0500

    Update scraper.py

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -1,6 +1,7 @@
 from lxml import html
 import requests
-page = requests.get('http://dartmouth.smartcatalogiq.com/en/current/orc/Departments-Programs-Undergraduate/Chemistry/CHEM-Chemistry-Undergraduate/CHEM-75')
+# See if adding the string at the end by using + '75' works
+page = requests.get('http://dartmouth.smartcatalogiq.com/en/current/orc/Departments-Programs-Undergraduate/Chemistry/CHEM-Chemistry-Undergraduate/CHEM-' + '75')
 tree = html.fromstring(page.content)
 prereq = tree.xpath('//a[@class="sc-courselink"]/text()')
 title = tree.xpath('//span/text()')
