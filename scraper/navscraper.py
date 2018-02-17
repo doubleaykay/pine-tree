e178fb02267c6a980671af6c1a441f3d7e4f8496
tiedyepanda458 <bridgerhh@gmail.com>
2018-02-17 10:23:28 -0500

    actually getting all the courses

--- scraper/navscraper.py
+++ scraper/navscraper.py
@@ -17,8 +17,8 @@ f.write("links = [\n")
 for link in links:
 	deptlinktree = getTree('http://dartmouth.smartcatalogiq.com'+link+'?getchildren=1')
 	deptlinks = deptlinktree.xpath('//a/@href');
-	if len(deptlinks) > 0:
-		depttree = getTree('http://dartmouth.smartcatalogiq.com'+deptlinks[len(deptlinks) - 1]+'?getchildren=1')
+	for link in deptlinks:
+		depttree = getTree('http://dartmouth.smartcatalogiq.com'+link+'?getchildren=1')
 		courselinks = depttree.xpath('//a/@href')
 		print courselinks
 		for l in courselinks:
--- scraper/scraper.py
+++ scraper/scraper.py
@@ -1,14 +1,13 @@
 from lxml import html
 import requests
 
+#import list of course links
+import links
+
 def print_class():
-   # Classes with no decimal numbering
-  # Initializes class number at 1
-   class_number = 1
-   while class_number <= 100:
     # Grabs the info from the page
-       page = requests.get('http://dartmouth.smartcatalogiq.com/en/current/orc/Departments-Programs-Undergraduate/Chemistry/CHEM-Chemistry-Undergraduate/CHEM-' + str(class_number))
-       class_number += 1
+    for link in links.links:
+       page = requests.get(link)
        tree = html.fromstring(page.content)
        title = tree.xpath('//span/text()')
        if title:
