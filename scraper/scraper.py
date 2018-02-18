391f602d063238f1fc8c7017b8c5dd3e944dd180
tiedyepanda458 <bridgerhh@gmail.com>
2018-02-18 01:01:53 -0500

    major scraper improvements, now prints prereqs, distribs, offered terms, title, and verbose title

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -5,22 +5,19 @@ import re
 #import list of course links
 import links
 
-def print_class():
-    # Grabs the info from the page
-    #for link in links.links:
-       page = requests.get('http://dartmouth.smartcatalogiq.com/en/current/orc/Departments-Programs-Undergraduate/Computer-Science/COSC-Computer-Science-Undergraduate/COSC-74')
-       tree = html.fromstring(page.content)
-       title = tree.xpath('//span/text()')
-       if title:
-          title.remove('\r\n\r\n\t')
-          title.remove(' >\r\n')
-          title.remove('\r\n\r\n\r\n')
-          #prereqs = tree.xpath('//a[@class="sc-courselink"]/text()')
-          #prereqs = tree.xpath('//h3[text()[contains(.,"Prerequisite")]]/following-sibling::node()/descendant-or-self::text()')
-          prereqs = tree.xpath('//div[@id="main"]/node()[count(preceding-sibling::h3) = 1]/descendant-or-self::text()')
-          pstring = "".join(prereqs[0:len(prereqs)-1])
-          print('T:' + str(title))
-          print(pstring)
-          print re.findall(r'[A-Z]{2,}\W+\d*\d',pstring)
-          
-print_class()
+# Grabs the info from the page
+#for link in links.links:
+page = requests.get('http://dartmouth.smartcatalogiq.com/en/current/orc/Departments-Programs-Undergraduate/Computer-Science/COSC-Computer-Science-Undergraduate/COSC-74')
+tree = html.fromstring(page.content)
+title = tree.xpath('//h1/span/text()')
+fulltitle = re.findall(r'\b.*\b',tree.xpath('//h1/span/following-sibling::text()')[0])[0]
+if title:
+  prereqs = tree.xpath('//div[@id="main"]/node()[count(preceding-sibling::h3) = 1]/descendant-or-self::text()')
+  pstring = "".join(prereqs[0:len(prereqs)-1])
+  offered = re.findall(r'\d\d\w',tree.xpath('//h3[text()="Offered"]/following-sibling::text()')[0])
+  distrib = re.findall(r'[A-Z]+',tree.xpath('//h3[contains(text(),"Distributive")]/following-sibling::text()')[0])
+  print(title[0])
+  print(fulltitle)
+  print(re.findall(r'[A-Z]{2,}\W+\d*\d',pstring))
+  print(offered)
+  print(distrib)
