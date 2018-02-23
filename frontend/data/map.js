0d037623372e0c1b13182cfb760c330d6844a962
doubleaykay <the.a.cars@gmail.com>
2018-02-23 15:52:10 -0500

    trying to filter nodes by id

--- frontend/data/map.js
+++ frontend/data/map.js
@@ -12,3 +12,36 @@ var cy = cytoscape({
     edges: edges,
   },
 });
+
+//cy.elements().remove();
+//var aaas = cy.nodes().data("id").includes("AAAS");
+//var aaas = $('id').includes('AAAS');
+//cy.elements().add(nodes.getElementById('MATH 013'));
+
+var allNodes = cy.nodes();
+cy.batch(function(){
+  allNodes.forEach(function( n ){
+    var type = n.data('NodeVisibility');
+    var id = n.data("id");
+
+    // remove the visible class from all nodes
+    var makeInVisible = function(){
+      n.removeClass('visible');
+    };
+
+    // make nodes visible again
+    var makeVisible = function(){
+      n.addClass('visible');
+    };
+
+    if ( id.includes("MATH") ){
+      makeVisible();
+    } else {
+      makeInVisible();
+    };
+
+    if( type != 'visible' ){
+      cy.elements().remove();
+    };
+  });
+});
